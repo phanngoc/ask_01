d6d5b552c34c494c4c63be08b89e7b66bb37b4b4
Your Name <lequidon.1993@gmail.com>
2017-02-19 11:22:51 +0700

    change file precompile rails

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,4 +12,4 @@ Rails.application.config.assets.version = '1.0'
 
 Rails.application.config.assets.precompile += %w( i18n.js )
 Rails.application.config.assets.precompile += %w( admin.css )
-Rails.application.config.assets.precompile += %w( ckeditor/* )
+Rails.application.config.assets.precompile += %w(ckeditor/config.js)
