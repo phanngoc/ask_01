edac4fc2128d98b853050fc9b99db9183bc9bc16
Phan Ngoc <lequidon.1993@gmail.com>
2017-02-28 11:42:19 +0700

    fix to deploy, include jsdiff, and style verque.scss

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -27,6 +27,7 @@
 //= require admin-lte
 //= require marked
 //= require simplemde/dist/simplemde.min.js
+//= require jsdiff
 //= require social-share-button
 //= require Chart.bundle
 //= require chartkick
